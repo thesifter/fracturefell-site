42dd853b549926318c8db39a673d77ab6caa7aed
thesifter <damen.hansen@gmail.com>
2025-05-16 20:52:11 +1200

    Add deply config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/fracturefell-site/',
   plugins: [
     vue(),
     vueDevTools(),
